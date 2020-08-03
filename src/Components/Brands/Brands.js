e0fec86e0e5e7a79f88aea57ab3b8177309baf3a
Carlos Oriol <oriol.carlos3@gmail.com>
2020-08-02 23:45:02 -0400

    Brand component done

--- src/Components/Brands/Brands.js
+++ src/Components/Brands/Brands.js
@@ -2,52 +2,92 @@ import React from 'react'
 import './brands.css'
 
 const Brands = () => (
-    <div class="brand-slider">
-        <div class="slide-track">
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+    <>
+
+        <div className='brands-header'>
+            <h1>Brands</h1>
+            <hr />
+        </div>
+
+        <div className='brand-content'>
+            <p className='brand-text'>At ABA Cycle &amp; Sports we sell products from many brands including the ones below and much more. Contact us if you want to know if we have a specific product.</p>
+
+            <div class="brand-slider">
+            <div class="slide-track">
+
+                <div class="slide">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Yamaha_Motor_logo.svg/1920px-Yamaha_Motor_logo.svg.png" alt="" />
+
+                </div>
+
+                <div class="slide">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Suzuki_logo_2.svg/1920px-Suzuki_logo_2.svg.png" alt="" />
+
+                </div>
+
+                <div class="slide" >
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/KTM-Logo.svg/1920px-KTM-Logo.svg.png" alt="" />
+
+                </div>
+
+                <div class="slide" >
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Kawasaki-logo.svg/1920px-Kawasaki-logo.svg.png" alt="" />
+
+                </div>
+
+                <div class="slide" >
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Polaris_Industries_logo.svg/1920px-Polaris_Industries_logo.svg.png" alt="" />
+                </div>
+
+                <div class="slide" >
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Motul_logo.svg/1920px-Motul_logo.svg.png" alt="" />
+                </div>
+
+                <div class="slide" >
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/71K%2Bot5WkDL._AC_SL1500_.jpg" alt="" />
+                </div>
+
+                <div class="slide">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Yamaha_Motor_logo.svg/1920px-Yamaha_Motor_logo.svg.png" alt="" />
+                </div>
+
+
+                <div class="slide" >
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Suzuki_logo_2.svg/1920px-Suzuki_logo_2.svg.png" alt="" />
+                </div>
+
+                <div class="slide" >
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/KTM-Logo.svg/1920px-KTM-Logo.svg.png" alt="" />
+                </div>
+
+                <div class="slide">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Kawasaki-logo.svg/1920px-Kawasaki-logo.svg.png" alt="" />
+                </div>
+
+                <div class="slide">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Polaris_Industries_logo.svg/1920px-Polaris_Industries_logo.svg.png" alt="" />
+                </div>
+
+                <div class="slide" >
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Motul_logo.svg/1920px-Motul_logo.svg.png" alt="" />
+                </div>
+
+                <div class="slide">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/71K%2Bot5WkDL._AC_SL1500_.jpg" alt="" />
+                </div>
+
+
             </div>
         </div>
-    </div>
+
+
+
+        </div>
+
+
+
+    </>
+
 )
 
 export default Brands 
