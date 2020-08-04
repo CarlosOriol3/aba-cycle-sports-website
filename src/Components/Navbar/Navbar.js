ac8099bf6757472376598f9a6fc4435334294dec
Carlos Oriol <oriol.carlos3@gmail.com>
2020-08-04 02:11:28 -0400

    navbar fix

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,8 @@ import './navbar.css'
 import NavigationItems from '../NavigationItems/NavigationItems'
 const Navbar = (props) =>
     <nav className='navbar'>
-        <img width='110px' src={process.env.PUBLIC_URL + '/abaLogo.png'} alt='ABA Cycle'/>
+        <h1>ABA Cycle &amp; Sports</h1>
+        <img src={process.env.PUBLIC_URL + '/abaLogo.png'} alt='ABA Cycle'/>
         <NavigationItems />
         <div className='ham' onClick={props.hamClicked}>
             <div className='ham-line'></div>
--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -15,12 +15,6 @@ const SideMenu = (props) => {
                 <a href='#brands' onClick={props.closed}><h2 className='menu-item'>Brands</h2></a>
                 <a href='#contact' onClick={props.closed}><h2 className='menu-item'>Contact</h2></a>
                 <a href='#location' onClick={props.closed}><h2 className='menu-item'>Location</h2></a>
-
-
-
-
-
-
             </div>
         </>
     )
