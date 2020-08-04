ecee91404342bbd62327803924f004acc84d185a
Carlos Oriol <oriol.carlos3@gmail.com>
2020-08-03 23:06:04 -0400

    mobile navigation fix

--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -9,12 +9,12 @@ const SideMenu = (props) => {
         <>
             <BackDrop show={props.show} clicked={props.closed} />
             <div className={`side-menu ${props.show ? 'open' : 'close'}`}  >
-                <a href='#home'><h2 className='menu-item'>Home</h2></a>
-                <a href='#about'><h2 className='menu-item'>About</h2></a>
-                <a href='#services'><h2 className='menu-item'>Services</h2></a>
-                <a href='#brands'><h2 className='menu-item'>Brands</h2></a>
-                <a href='#contact'><h2 className='menu-item'>Contact</h2></a>
-                <a href='#location'><h2 className='menu-item'>Location</h2></a>
+                <a href='#home' onClick={props.closed}><h2 className='menu-item'>Home</h2></a>
+                <a href='#about' onClick={props.closed}><h2 className='menu-item'>About</h2></a>
+                <a href='#services' onClick={props.closed}><h2 className='menu-item'>Services</h2></a>
+                <a href='#brands' onClick={props.closed}><h2 className='menu-item'>Brands</h2></a>
+                <a href='#contact' onClick={props.closed}><h2 className='menu-item'>Contact</h2></a>
+                <a href='#location' onClick={props.closed}><h2 className='menu-item'>Location</h2></a>
 
 
 
