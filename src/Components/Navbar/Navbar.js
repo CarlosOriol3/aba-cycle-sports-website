acb0db19bb18e00fc069dee99f872c31efc2328a
Carlos Oriol <oriol.carlos3@gmail.com>
2020-10-01 03:13:29 -0400

    Readme

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './navbar.css'
 import NavigationItems from '../NavigationItems/NavigationItems'
 const Navbar = (props) =>
     <nav className='navbar'>
-        <a href='#home' className='header-text'><h1>ABA Cycle &amp; Sports</h1></a>
+        <a href='#home' className='header-text'><h1><span>ABA Cycle &amp; Sports</span></h1></a>
         <a href='#home'><img src={process.env.PUBLIC_URL + '/abaLogo.png'} alt='ABA Cycle'/></a>
         <NavigationItems />
         <div className='ham' onClick={props.hamClicked}>
